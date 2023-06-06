a01605826b60b03943c08e8e467177ffc9855649
kennymath <112353288+kennymath@users.noreply.github.com>
2023-06-05 20:17:22 -0400

    Update docker_compose.sh

--- docker_compose.sh
+++ docker_compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.18.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
